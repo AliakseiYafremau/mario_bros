9a4ab37cd5f1cb4e9b6812d714ff1fa00af0c8d2
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-11-07 15:42:33 +0000

    Correct player movement

--- game/domain/game.py
+++ game/domain/game.py
@@ -21,7 +21,7 @@ class Game:
 
         for player in players.keys():
             is_correct = False
-            if (player.x, player.y) in players[player]:
+            if Floor(player.x, player.y) in players[player]:
                 is_correct = True
             if not is_correct:
                 raise DomainError("player is not located on one of the entered points")
@@ -46,30 +46,30 @@ class Game:
 
     def move_player_up(self, player: Player):
         player_positions = self.players_positions[player]
+        player_current_floor = Floor(player.x, player.y)
 
-        for position_index, floor in enumerate(player_positions):
-            if floor.x == player.x and floor.y == player.y:
-                if position_index == len(player_positions) - 1:
-                    raise DomainError("cannot raise the player because is on the top")
+        if player_current_floor == player_positions[-1]:
+            raise DomainError("cannot raise the player because is on the top")
 
-                next_floor = player_positions[position_index + 1]
-                player.x, player.y = next_floor.x, next_floor.y
+        for position_index in range(len(player_positions) - 1):
+            if player_current_floor == player_positions[position_index]:
+                new_player_position = player_positions[position_index + 1]
+                player.x, player.y = new_player_position.x, new_player_position.y
                 return
 
         raise DomainError("player has invalid position")
 
     def move_player_down(self, player: Player):
         player_positions = self.players_positions[player]
+        player_current_floor = Floor(player.x, player.y)
 
-        for position_index, floor in enumerate(player_positions):
-            if floor.x == player.x and floor.y == player.y:
-                if position_index == 0:
-                    raise DomainError(
-                        "cannot lower the player because is on the bottom"
-                    )
+        if player_current_floor == player_positions[0]:
+            raise DomainError("cannot lower the player because is on the bottom")
 
-                previous_floor = player_positions[position_index - 1]
-                player.x, player.y = previous_floor.x, previous_floor.y
+        for position_index in range(1, len(player_positions)):
+            if player_current_floor == player_positions[position_index]:
+                new_player_position = player_positions[position_index - 1]
+                player.x, player.y = new_player_position.x, new_player_position.y
                 return
 
         raise DomainError("player has invalid position")
